d7ced46959e29f1a5f5680aeb8ab7e03941bc83d
kv4biz <kv4biz@gmail.com>
2024-09-14 11:18:02 -0800

    fix text heading

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,8 @@ async function Home() {
   const data = await fetchAnime(1);
   return (
     <main className="sm:p-16 py-16 px-8 flex flex-col gap-10">
-      <div className="flex justify-between items-center">
-        <div className="text-3xl text-white font-bold">Explore Anime</div>
-        <div className="text-3xl text-white font-bold">By Popularity</div>
+      <div className="text-md md:text-lg lg:text-xl text-white font-semibold md:font-bold">
+        Explore Anime
       </div>
 
       <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
